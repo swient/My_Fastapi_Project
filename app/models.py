25feab8668837fd260886ba4eef9ca37fe9ecd07
swient <B11217048@yuntech.edu.tw>
2024-12-19 14:50:07 +0800

    feat: Reinitialize Todo and add priority field

--- app/models.py
+++ app/models.py
@@ -10,6 +10,7 @@ class Todo(Base):
     description = Column(String, nullable=True)
     completed = Column(Boolean, nullable=False)
     due_date = Column(Date, nullable=True)
+    priority = Column(Integer, default=1)
 
 class User(Base):
     __tablename__ = "users"
